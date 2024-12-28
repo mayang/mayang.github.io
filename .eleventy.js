a7e17872d554b85d8502d8635d5c9736a4f22137
may ang <mayday815@gmail.com>
2024-12-27 17:40:18 -0800

    redoing some stuff

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,11 @@
-module.exports = function(eleventyConfig) {
-  // input dir????
-  // output dir _site
+// Any combination of these
+import { I18nPlugin, RenderPlugin, HtmlBasePlugin } from "@11ty/eleventy";
 
-
+export default function (eleventyConfig) {
+  // …
   eleventyConfig.addPassthroughCopy("src/CNAME");
 
   return {
 
   }
-}
+};
