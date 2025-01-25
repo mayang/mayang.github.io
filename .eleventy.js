eb28112d1252e2cff6d729fd12dc8f6d76ccefd2
may ang <mayday815@gmail.com>
2025-01-25 13:56:57 -0800

    stuff

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,23 @@
 // Any combination of these
-import { I18nPlugin, RenderPlugin, HtmlBasePlugin } from "@11ty/eleventy";
+// import { I18nPlugin, RenderPlugin, HtmlBasePlugin } from "@11ty/eleventy";
 
 export default function (eleventyConfig) {
   // …
   eleventyConfig.addPassthroughCopy("src/CNAME");
+  eleventyConfig.setServerOptions({
+    watch: ['_site/**/*.css'],
+  });
+
+  // for personal styles instead of tailwind
+  // eleventyConfig.addPassthroughCopy("./src/styles/");
+  // eleventyConfig.addWatchTarget("./src/styles/");
 
   return {
-
-  }
+    dir: {
+      input: "src",
+      // includes: "_includes",
+      // data: "_data",
+      // output: "_site",
+    },
+  };
 };
