16c32fb6b16c023bcfef74ad829165820d820899
may ang <mayday815@gmail.com>
2025-03-07 17:09:01 -0800

    using base 11ty blog basics

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,32 @@
 // Any combination of these
 // import { I18nPlugin, RenderPlugin, HtmlBasePlugin } from "@11ty/eleventy";
 
+import pluginNavigation from "@11ty/eleventy-navigation";
+
+
 export default function (eleventyConfig) {
   // …
-  eleventyConfig.addPassthroughCopy("src/CNAME");
-  eleventyConfig.setServerOptions({
-    watch: ['_site/**/*.css'],
+  // eleventyConfig.addPassthroughCopy("src/CNAME");
+  eleventyConfig.addPassthroughCopy({
+    "./public/": "/"
   });
+
+  eleventyConfig.addBundle("css", {
+    toFileDirectory: "dist",
+  });
+
+  // eleventyConfig.addPlugin(pluginNavigation);
+
+  // eleventyConfig.setServerOptions({
+  //   watch: ['_site/**/*.css'],
+  // });
+  // eleventyConfig.addWatchTarget('./theme/**/*.{css,js}');
+
+  // const cssTemplate = fs.readFileSync(path.resolve('theme/css/', 'bundle.liquid'), 'utf-8');
+  // const jsTemplate = fs.readFileSync(path.resolve('theme/js/', 'bundle.njk'), 'utf-8');
+
+  // eleventyConfig.addTemplate('css-bundle.liquid', cssTemplate);
+  // eleventyConfig.addTemplate('js-bundle.njk', jsTemplate);
 
   // for personal styles instead of tailwind
   // eleventyConfig.addPassthroughCopy("./src/styles/");
@@ -14,10 +34,11 @@ export default function (eleventyConfig) {
 
   return {
     dir: {
-      input: "src",
-      // includes: "_includes",
-      // data: "_data",
-      // output: "_site",
+      input: "content",
+      includes: "../_includes",
+      data: "../_data",
+      output: "_site",
+      // layouts: "_includes/layouts"
     },
   };
 };
